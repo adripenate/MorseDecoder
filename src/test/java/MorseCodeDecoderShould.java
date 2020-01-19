f7582c74a9b8a3623240fa3f3b6bf34fb3a2c58b
adriana <adrianapenate98@gmail.com>
2020-01-19 10:04:20 +0000

    Red test - decode_a_letter

---
+++ src/test/java/MorseCodeDecoderShould.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MorseCodeDecoderShould {
+    @Test
+    public void decode_a_letter() {
+        assertThat(MorseCodeDecoder.decode(".-")).isEqualTo("A");
+    }
+
+    private static class MorseCodeDecoder {
+        public static String decode(String code) {
+            return null;
+        }
+    }
+}
