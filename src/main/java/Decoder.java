76ec6f1bc445e5255ecd51cf65bb78cd7ed97528
adriana <adrianapenate98@gmail.com>
2020-01-19 10:31:20 +0000

    Refactor - Map moved to its own file

---
+++ src/main/java/Decoder.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Decoder {
+    public static Map<String, String> decoder = new HashMap<String, String>(){{
+        put(".-", "A");
+        put("-...", "B");
+        put("-.-.", "C");
+        put("-..", "D");
+        put(".", "E");
+        put("..-.", "F");
+        put("--.-", "G");
+        put("....", "H");
+        put("..", "I");
+        put(".---", "J");
+        put("-.-", "K");
+        put(".-..", "L");
+        put("--", "M");
+        put("-.", "N");
+        put("---", "O");
+        put(".--.", "P");
+        put("--.-", "Q");
+        put(".-.", "R");
+        put("...", "S");
+        put("-", "T");
+        put("..-", "U");
+        put("...-", "V");
+        put(".--", "W");
+        put("-..-", "X");
+        put("-.--", "Y");
+        put("--..", "Z");
+    }};
+}
--- src/test/java/MorseCodeDecoderShould.java
+++ src/test/java/MorseCodeDecoderShould.java
@@ -1,8 +1,6 @@
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,34 +17,6 @@ public class MorseCodeDecoderShould {
     }
 
     private static class MorseCodeDecoder {
-        private static Map<String, String> decoder = new HashMap<String, String>(){{
-            put(".-", "A");
-            put("-...", "B");
-            put("-.-.", "C");
-            put("-..", "D");
-            put(".", "E");
-            put("..-.", "F");
-            put("--.-", "G");
-            put("....", "H");
-            put("..", "I");
-            put(".---", "J");
-            put("-.-", "K");
-            put(".-..", "L");
-            put("--", "M");
-            put("-.", "N");
-            put("---", "O");
-            put(".--.", "P");
-            put("--.-", "Q");
-            put(".-.", "R");
-            put("...", "S");
-            put("-", "T");
-            put("..-", "U");
-            put("...-", "V");
-            put(".--", "W");
-            put("-..-", "X");
-            put("-.--", "Y");
-            put("--..", "Z");
-        }};
 
         public static String decode(String code) {
             return Arrays.stream(code.split(" "))
@@ -55,7 +25,7 @@ public class MorseCodeDecoderShould {
         }
 
         private static String translate(String letter) {
-            return decoder.get(letter);
+            return Decoder.decoder.get(letter);
         }
     }
 }
